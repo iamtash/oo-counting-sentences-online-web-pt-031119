09d0e6c15f2af98508a6d4e32ddb32ad833e7269
Natasha Chernis <natashachernis@gmail.com>
2019-04-02 02:48:08 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,12 +3,12 @@ require 'pry'
 class String
 
   def sentence?
-    binding.pry
     self.end_with?(".")
   end
 
   def question?
-
+    binding.pry
+    self.!end_with("?")
   end
 
   def exclamation?
